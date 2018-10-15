2dc045d77df69b37c3d8f26c78b6e1e8e9468aec
KatagiriSo <clementec@clementecMacBookPro.local>
2018-10-15 18:43:24 +0900

    add adjust func

--- README.md
+++ README.md
@@ -3,15 +3,24 @@ It is a program to transform Diagram used in Categorical Theory to LaTex (xymatr
 
 ```swift
 func sample() {
-    let a = CObject("a")
-    let b = CObject("b")
-    let c = CObject("c")
-    let d = CObject("d")
+     let z = CObject("Z")
+
+    let a = CObject("A")
+    let b = CObject("B")
+    let c = CObject("C")
+    let d = CObject("D")
+    
+    z.addDownRight(a,dx:1)
 
     a.addRight(b).setArrow("f").addDown(d).setArrow("g")
     a.addDown(c).setArrow("h").addRight(d).setArrow("k")
     
-    let cat = Category(objects:[a,b,c,d])
+    z.to(a).setArrow("l1")
+    z.to(b).setArrow("l2",attr:"@(ur,dr)")
+    z.to(c).setArrow("l3")
+    z.to(d).setArrow("l4")
+    
+    let cat = Category(objects:[z,a,b,c,d])
     let mat = matrix(cat)
     display(matrix:mat.matrix, matrixsize:mat.maxsize, arrows:mat.arrows)
 }
@@ -22,7 +31,10 @@ func sample() {
 # Output
 ```latex
 \xymatrix{
-a \ar[r]_{f}^{} \ar[d]_{h}^{}  & b \ar[d]_{g}^{}  \\
-c \ar[r]_{k}^{}  & d
+Z \ar[rrd]_{l1}^{} \ar@(ur,dr)[rrrd]_{l2}^{} \ar[rrdd]_{l3}^{} \ar[rrrdd]_{l4}^{}  &    &    &    \\
+   &    & A \ar[r]_{f}^{} \ar[d]_{h}^{}  & B \ar[d]_{g}^{}  \\
+   &    & C \ar[r]_{k}^{}  & D
 }
 ```
+
+<img src="samplepng.png">
--- rdcategorywriter.swift
+++ rdcategorywriter.swift
@@ -8,6 +8,14 @@
 
 typealias Vec = (x:Int,y:Int)
 
+let debugMode = true
+
+func debugLog(_ str:String) {
+    if debugMode {
+        print(str)
+    }
+}
+
 struct Category {
     var objects:[CObject]
     func arrows()->[CArrow] {
@@ -24,6 +32,7 @@ struct Category {
                 y = $0.location.y
             }
         }
+//        debugLog("max size\((x+1,y+1))")
         return (x:x+1,y:y+1)
     }
 }
@@ -41,58 +50,89 @@ class CObject {
         CObject.currentid = CObject.currentid + 1
     }
     
-    func addRight(_ o:CObject) -> CObject {
-        o.location = (x:self.location.x+1, y:self.location.y)
+    func addRight(_ o:CObject, dx:Int=0,dy:Int=0) -> CObject {
+        o.location = (x:self.location.x+1+dx, y:self.location.y+dy)
         self.tempRelation = o
         return self
     }
     
-    func addDown(_ o:CObject) -> CObject {
-        o.location = (x:self.location.x, y:self.location.y+1)
+    func addDown(_ o:CObject, dx:Int=0,dy:Int=0) -> CObject {
+        o.location = (x:self.location.x+dx, y:self.location.y+1+dy)
         self.tempRelation = o
         return self
     }
     
-    func addUp(_ o:CObject) -> CObject {
-        o.location = (x:self.location.x, y:self.location.y-1)
+    func addUp(_ o:CObject, dx:Int=0,dy:Int=0) -> CObject {
+        o.location = (x:self.location.x+dx, y:self.location.y-1+dy)
         self.tempRelation = o
         return self
     }
     
-    func addLeft(_ o:CObject) -> CObject {
-        o.location = (x:self.location.x-1, y:self.location.y)
+    func addLeft(_ o:CObject, dx:Int=0,dy:Int=0) -> CObject {
+        o.location = (x:self.location.x-1+dx, y:self.location.y+dy)
         self.tempRelation = o
         return self
     }
     
-    func setArrow(_ name:String, isUpper:Bool = true) -> CObject {
+    func addUpRight(_ o:CObject, dx:Int=0,dy:Int=0) -> CObject {
+        o.location = (x:self.location.x+1+dx, y:self.location.y-1+dy)
+        self.tempRelation = o
+        return self
+    }
+    
+    func addDownRight(_ o:CObject, dx:Int=0,dy:Int=0) -> CObject {
+        o.location = (x:self.location.x+1+dx, y:self.location.y+1+dy)
+        self.tempRelation = o
+        return self
+    }
+    
+    func addDownLeft(_ o:CObject, dx:Int=0,dy:Int=0) -> CObject {
+        o.location = (x:self.location.x-1+dx, y:self.location.y+1+dy)
+        self.tempRelation = o
+        return self
+    }
+    
+    func addUpLeft(_ o:CObject, dx:Int=0,dy:Int=0) -> CObject {
+        o.location = (x:self.location.x-1+dx, y:self.location.y-1+dy)
+        self.tempRelation = o
+        return self
+    }
+    
+    func setArrow(_ name:String, isUpper:Bool = true, attr:String = "") -> CObject {
         if let t = self.tempRelation {
             let up = (isUpper==true) ? name : ""
             let down = (isUpper==true) ? "" : name
-            let a = CArrow(dom:self, cod:t, up:up, down:down)
+            let a = CArrow(dom:self, cod:t, up:up, down:down, attr:attr)
             self.arrows.append(a)
             return t
         }
         let up = (isUpper==true) ? name : ""
         let down = (isUpper==true) ? "" : name
-        let a = CArrow(dom:self, cod:self, up:up, down:down)
+        let a = CArrow(dom:self, cod:self, up:up, down:down, attr:attr)
         self.arrows.append(a)
+        return self
+    }
+    
+    func to(_ ob:CObject) -> CObject {
+        self.tempRelation = ob
         return self
     }
 }
 
 class CArrow {
-    init(dom:CObject, cod:CObject, up:String="", down:String="") {
+    init(dom:CObject, cod:CObject, up:String="", down:String="", attr:String = "") {
         self.dom = dom
         self.cod = cod
         self.upperComment = up
         self.underComment = down
+        self.attr = attr
     }
     var type:String = ""
     var upperComment:String
     var underComment:String
     var dom:CObject
     var cod:CObject
+    var attr:String
 }
 
 
@@ -106,7 +146,7 @@ func matrix(_ category:Category) -> (matrix:[String:(Int,Int)], maxsize:Vec, arr
     }
     
     let ars = category.arrows().map {
-        [$0.dom.name, $0.cod.name, $0.upperComment, $0.underComment]
+        [$0.dom.name, $0.cod.name, $0.upperComment, $0.underComment, $0.attr]
         }.map {Arrow($0)}
     return (matrix:matrix, maxsize:maxsize, arrows:ars)
 }
@@ -117,12 +157,14 @@ struct Arrow {
     let last:String
     let d_comment:String
     let u_comment:String
+    let attr:String
     
     init(_ list:[String]) {
         first = list[0]
         last = list[1]
         d_comment = list[2]
         u_comment = list[3]
+        attr = list[4]
     }
 }
 
@@ -165,10 +207,9 @@ func ar(arrow:Arrow, matrix:[String:(Int,Int)]) -> String {
         }
         return ""
     }()
+
     
-    
-    
-    let tot = "\\ar[" + hor + ver + "]" 
+    let tot = "\\ar\(arrow.attr)[" + hor + ver + "]"
         + "_" + "{" + arrow.d_comment + "}"
         + "^" + "{" + arrow.u_comment + "}"
     
@@ -179,24 +220,24 @@ func ar(arrow:Arrow, matrix:[String:(Int,Int)]) -> String {
 func display(matrix:[String:(Int,Int)],matrixsize:(Int,Int),arrows:[Arrow]) {
     print("\\xymatrix{")
     
-    for a in 0..<(matrixsize.0) {
+    for a in 0..<(matrixsize.1) {
         var list:[String] = []
-        //print("\(a)")
-        for b in 0..<(matrixsize.1) {
-            //print("\(b)")
+//        debugLog("a=\(a)")
+        for b in 0..<(matrixsize.0) {
+//            debugLog("b=\(b)")
             let key = search(matrix,b,a)
-            //print("\(key)")
+//            debugLog("\(key)")
             let ars = arrows.filter {$0.first == key}
             let result = ars.map {
                 ar(arrow:$0, matrix:matrix)
                 }.joined(separator:" ")
             
-            let last = matrixsize.1 - 1
+            let last = matrixsize.0 - 1
             let _and_ = b != last ? " & " : ""
             list.append("\(key) \(result) \(_and_)")
         }
         
-        let last = matrixsize.0 - 1
+        let last = matrixsize.1 - 1
         let _bs_ = a != last ? " \\\\" : ""
         list.append("\(_bs_)")
         print("\(list.joined(separator:""))")
@@ -275,15 +316,24 @@ func templetaeEnd() {
 //////
 
 func sample() {
-    let a = CObject("a")
-    let b = CObject("b")
-    let c = CObject("c")
-    let d = CObject("d")
+    let z = CObject("Z")
+
+    let a = CObject("A")
+    let b = CObject("B")
+    let c = CObject("C")
+    let d = CObject("D")
+    
+    z.addDownRight(a,dx:1)
 
     a.addRight(b).setArrow("f").addDown(d).setArrow("g")
     a.addDown(c).setArrow("h").addRight(d).setArrow("k")
     
-    let cat = Category(objects:[a,b,c,d])
+    z.to(a).setArrow("l1")
+    z.to(b).setArrow("l2",attr:"@(ur,dr)")
+    z.to(c).setArrow("l3")
+    z.to(d).setArrow("l4")
+    
+    let cat = Category(objects:[z,a,b,c,d])
     let mat = matrix(cat)
     display(matrix:mat.matrix, matrixsize:mat.maxsize, arrows:mat.arrows)
 }
@@ -291,3 +341,7 @@ func sample() {
 templetaeBegin()
 sample()
 templetaeEnd()
+
+
+
+
